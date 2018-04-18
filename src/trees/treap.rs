3098bd68cc1e84a409929ce4cce498768a133c85
mbankim <mkim@wish.com>
2018-04-17 18:25:54 -0700

    treap insert; tests

--- src/trees/treap.rs
+++ src/trees/treap.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::mem;
 use rand::random;
 
 type Tree = Option<Box<Node>>;
@@ -13,7 +14,7 @@ impl Treap {
     }
 
     pub fn insert(&mut self, val: u32) {
-        insert_node(&mut self.root, val);
+        self.root = Some(insert_node(&mut self.root, val));
     }
 
     pub fn remove(&mut self, val: u32) -> u32 {
@@ -55,17 +56,47 @@ impl Node {
     }
 }
 
-fn insert_node(tree: &mut Tree, val: u32) {
-    match *tree {
-        Some(ref mut node) => {
+fn insert_node(tree: &mut Tree, val: u32) -> Box<Node> {
+    let mut cur_tree = tree.take();
+    cur_tree.map_or(
+        Box::new(Node::new(val)),
+        |mut node| {
             match val.cmp(&node.val) {
-                Ordering::Less => insert_node(&mut node.left, val),
-                Ordering::Greater => insert_node(&mut node.right, val),
-                Ordering::Equal => return,
+                Ordering::Less => {
+                    let mut new_node = insert_node(&mut node.left, val);
+                    match new_node.priority.cmp(&node.priority) {
+                        Ordering::Less => {
+                            // rotate
+                            let mut right_child = new_node.right.take();
+                            mem::replace(&mut node.left, right_child);
+                            mem::replace(&mut new_node.right, Some(node));
+                            new_node
+                        },
+                        Ordering::Greater | Ordering::Equal => {
+                            node.left = Some(new_node);
+                            node
+                        },
+                    }
+                }
+                Ordering::Greater | Ordering::Equal => {
+                    let mut new_node = insert_node(&mut node.right, val);
+                    match new_node.priority.cmp(&node.priority) {
+                        Ordering::Less => {
+                            // rotate
+                            let mut left_child = new_node.left.take();
+                            mem::replace(&mut node.right, left_child);
+                            mem::replace(&mut new_node.left, Some(node));
+                            new_node
+                        },
+                        Ordering::Greater | Ordering::Equal => {
+                            node.right = Some(new_node);
+                            node
+                        },
+                    }
+                }
             }
-        },
-        None => *tree = Some(Box::new(Node::new(val))),
-    }
+        }
+    )
 }
 
 fn get_inorder(tree: &Tree) -> Vec<u32> {
@@ -96,7 +127,15 @@ fn count_depth(tree: &Tree, depth: u32) -> Vec<u32> {
 
 #[cfg(test)]
 mod tests {
-    use super::{Treap};
+    use super::{Treap, Node};
+
+    fn assert_heap_property(tree: &Option<Box<Node>>, parent_priority: u32) {
+        if let Some(ref node) = *tree {
+            assert!(parent_priority <= node.priority);
+            assert_heap_property(&node.right, node.priority);
+            assert_heap_property(&node.left, node.priority);
+        }
+    }
 
     #[test]
     fn test_treap_insert() {
@@ -105,5 +144,6 @@ mod tests {
             treap.insert(i);
         }
         assert_eq!(treap.inc_order(), (0u32..100u32).collect::<Vec<u32>>());
+        assert_heap_property(&treap.root, 0);
     }
 }
